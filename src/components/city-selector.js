623bcd0197f8b414b0e901d1ba9d465780eafa9e
rinatsakaev <r.sakaevv@gmail.com>
2020-05-24 00:13:32 +0500

    keyboard hover

--- src/components/city-selector.js
+++ src/components/city-selector.js
@@ -5,26 +5,33 @@ import useSessionState from '../hooks/session-state';
 function CitySelector() {
     const cities = useContext(SelectorContext);
     const [selectedCitiesList, changeSelectedCitiesList] = useSessionState('list', []);
-    const [selectedCity, setSelectedCity] = useState(undefined);
+
+    const [activeCity, _setActiveCity] = useState(undefined);
+    const activeCityRef = useRef(activeCity);
+    const setActiveCity = (x) => {
+        activeCityRef.current = x;
+        _setActiveCity(x);
+    };
+
     const [predictedCities, _setPredictedCities] = useState([]);
     const predictedCitiesRef = useRef(predictedCities);
     const setPredictedCities = (x) => {
-      predictedCitiesRef.current = x;
-      _setPredictedCities(x);
+        predictedCitiesRef.current = x;
+        _setPredictedCities(x);
     };
 
     const inputRef = useRef(null);
 
     const addCityToList = () => {
-        if (!selectedCity || selectedCitiesList.findIndex(x => x.id === selectedCity.id) !== -1)
+        if (!activeCity || selectedCitiesList.findIndex(x => x.id === activeCity.id) !== -1)
             return;
-        changeSelectedCitiesList((prevState) => [...prevState, selectedCity]);
+        changeSelectedCitiesList((prevState) => [...prevState, activeCity]);
         setPredictedCities([]);
         inputRef.current.value = '';
     };
 
     const selectCity = (id, name) => {
-        setSelectedCity({id, name});
+        setActiveCity({id, name});
     };
 
     const handleInput = (e) => {
@@ -43,22 +50,20 @@ function CitySelector() {
     };
 
     const handleKeyPress = (e) => {
-        if (predictedCitiesRef.current.length === 0 || (e.key !==  'ArrowUp'&& e.key !== 'ArrowDown'))
+        if (predictedCitiesRef.current.length === 0 || (e.key !== 'ArrowUp' && e.key !== 'ArrowDown'))
             return;
 
-        if (!selectedCity){
-            setSelectedCity(predictedCitiesRef.current[0]);
+        if (!activeCityRef.current) {
+            setActiveCity(predictedCitiesRef.current[0]);
             return;
         }
 
-        const selectedCityIndex = predictedCitiesRef.current.findIndex(x => x.id === selectedCity.id);
-        if (e.key === 'ArrowUp')
-            setSelectedCity(predictedCitiesRef.current[(selectedCityIndex + 1) % predictedCitiesRef.current.length]);
+        const selectedCityIndex = predictedCitiesRef.current.findIndex(x => x.id === activeCityRef.current.id);
+        if (e.key === 'ArrowUp' && selectedCityIndex > 0)
+            setActiveCity(predictedCitiesRef.current[selectedCityIndex - 1]);
 
-        if (e.key === 'ArrowDown')
-            setSelectedCity(predictedCitiesRef.current[(selectedCityIndex - 1) % predictedCitiesRef.current.length]);
-        
-
+        if (e.key === 'ArrowDown' && selectedCityIndex < predictedCitiesRef.current.length - 1)
+            setActiveCity(predictedCitiesRef.current[selectedCityIndex + 1]);
     };
 
     useEffect(() => {
@@ -68,15 +73,17 @@ function CitySelector() {
 
 
     useEffect(() => {
-        if (selectedCity)
-            inputRef.current.value = selectedCity.name;
-    }, [selectedCity]);
+        if (activeCity)
+            inputRef.current.value = activeCity.name;
+    }, [activeCity]);
 
     const selectedCities = selectedCitiesList.map(x =>
         <li key={x.id} className="selected-cities__item">{x.name}</li>);
+
     const citiesItems = predictedCities.map(x =>
-        <li key={x.id} className="city-selector__item"
-            onClick={() => selectCity(x.id, x.name)}>{x.name}</li>);
+        <li key={x.id} className={`city-selector__item ${activeCity && activeCity.id === x.id ? 'hover' : null}`}
+            onClick={() => selectCity(x.id, x.name)}>{x.name}</li>
+    );
 
     return (
         <React.Fragment>
